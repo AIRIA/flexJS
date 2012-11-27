1efe1eefb4d4eb4e6543761335a959340be4382b
airia <chaibingcheng0305@163.com>
2012-11-27 13:16:06 +0800

    添加遮罩类

---
+++ com/airia/masks/RectMask.js
@@ -0,0 +1,26 @@
+(function(){
+	/**
+	 * 矩形的遮罩区域
+	 */
+	Flex.RectMask = function(config){
+		this.x = config.x || 0;
+		this.y = config.y || 0;
+		this.width = config.width;
+		this.height = config.height;
+	}
+	
+	Flex.RectMask.prototype = {
+		constructor:Flex.RectMask,
+		start:function(){
+			context.save();
+			context.beginPath();
+			context.rect(this.x,this.y,this.width,this.height);
+			context.clip();
+			context.closePath();
+		},
+		end:function(){
+			context.restore();
+		}
+	}
+	
+})();
---
+++ com/airia/masks/RoundMask.js
@@ -0,0 +1,24 @@
+(function(){
+	/**
+	 * 圆形的遮罩区域
+	 */
+	Flex.RoundMask = function(config){
+		this.x = config.x || 0;
+		this.y = config.y || 0;
+		this.radius = config.radius;
+	}
+	
+	Flex.RoundMask.prototype = {
+		constructor:Flex.RoundMask,
+		start:function(){
+			context.save();
+			context.beginPath();
+			context.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+			context.clip();
+			context.closePath();
+		},
+		end:function(){
+			context.restore();
+		}
+	}
+})();
