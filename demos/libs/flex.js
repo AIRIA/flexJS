041183b2ceda89fea3562ba98c974bb979c17c84
airia <chaibingcheng0305@163.com>
2012-12-03 13:39:43 +0800

    完善group 添加了每个属性的set/get方法

--- demos/libs/flex.js
+++ demos/libs/flex.js
@@ -404,6 +404,7 @@
 				}
 				return false;
 			}else if(x>this.stageX&&x<(this.stageX+this.explicitOrMeasureWidth)&&y>this.stageY&&y<(this.stageY+this.explicitOrMeasureHeight)){
+				trace(this);
 				return true;
 			}
 			return false;
@@ -1290,62 +1291,143 @@ var TimerEvent = {
 
 })();
 //---------------------------------------------
-(function(){
+(function() {
 	/**
 	 * @author AIRIA
 	 * @class Flex.Group
-	 * @constructor 
+	 * @constructor
 	 * @description
-	 * @param {JSON} 
-	 * 
+	 * @param {JSON}
+	 *
 	 */
-	Flex.Group = function(config){
-		Flex.extend(this,new Flex.Sprite(config),config);
+	Flex.Group = function(config) {
+		Flex.extend(this, new Flex.Sprite(config), config);
 		/**
 		 * @description 布局的方向 默认是垂直方向  【vertical|horizontal】
 		 * @field
 		 */
-		this.direction = "vertical";
+		this._direction = "vertical";
 		/**
-		 * @description 垂直布局中的行间距 
+		 * @description 垂直布局中的行间距
 		 * @field
 		 */
-		this.verticalGap = 5;
+		this._verticalGap = 5;
 		/**
 		 * @description 水平布局中的列间距
 		 * @field
 		 */
-		this.horizontalGap = 5;
+		this._horizontalGap = 5;
 		/**
 		 * @description 水平对齐容器中的子容器。可能值包括 "left"、"center" 和 "right"。默认值为 "left"
 		 * @field
 		 */
-		this.horizontalAlign = "left";
+		this._horizontalAlign = "left";
 		/**
 		 * @description 垂直对齐容器中的子项。可能值包括 "top"、"middle" 和 "bottom"。默认值为 "top"
 		 * @field
 		 */
-		this.verticalAlign = "top";
-		
-		this.paddingTop = 0;
-		this.paddingLeft = 0;
-		this.paddingBottom = 0;
-		this.paddingRight = 0;
+		this._verticalAlign = "top";
+
+		this._paddingTop = 0;
+		this._paddingLeft = 0;
+		this._paddingBottom = 0;
+		this._paddingRight = 0;
+
+		Object.defineProperties(this, {
+			direction : {
+				get : function() {
+					return this._direction;
+				},
+				set : function(value) {
+					if(this._direction != value) {
+						this._direction = value;
+						this.updateDisplayList();
+					}
+				}
+			},
+			verticalGap : {
+				get : function() {
+					return this._verticalGap;
+				},
+				set : function(value) {
+					if(this._verticalGap != value) {
+						this._verticalGap = value;
+						this.updateDisplayList();
+					}
+				}
+			},
+			horizontalGap : {
+				get : function() {
+					return this._horizontalGap;
+				},
+				set : function(value) {
+					if(this._horizontalGap != value) {
+						this._horizontalGap = value;
+						this.updateDisplayList();
+					}
+				}
+			},
+			paddingLeft : {
+				get : function() {
+					return this._paddingLeft
+				},
+				set : function(value) {
+					if(this._paddingLeft != value) {
+						this._paddingLeft = value;
+						this.updateDisplayList();
+					}
+				}
+			},
+			paddingRight : {
+				get : function() {
+					return this._paddingRight;
+				},
+				set : function(value) {
+					if(this._paddingRight != value) {
+						this._paddingRight = value;
+						this.updateDispalyList();
+					}
+				}
+			},
+			paddingTop : {
+				get : function() {
+					return this._paddingTop;
+				},
+				set : function(value) {
+					if(this._paddingTop != value) {
+						this._paddingTop = value;
+						this.updateDisplayList();
+					}
+				}
+			},
+			paddingBottom : {
+				get : function() {
+					return this._paddingBottom;
+				},
+				set : function(value) {
+					if(this._paddingBottom != vlaue) {
+						this._paddingBottom = value;
+						this.updateDisplayList();
+					}
+				}
+			}
+		})
+
 	}
-	
+
 	Flex.Group.prototype = {
-		constructor:Flex.Group,
+		constructor : Flex.Group,
 		/**
 		 * [override]
 		 */
-		addChild:function(child){
-			this.superClass.addChild.apply(this,[child]);
+		addChild : function(child) {
+			this.superClass.addChild.apply(this, [child]);
 			this.updateDisplayList();
 		},
 		/**
 		 * 进行布局
 		 */
-		updateDisplayList:function(){
+		updateDisplayList : function() {
 			this.measureHeight = 0;
 			this.measureWidth = 0;
 			var children = this.getChildren();
@@ -1353,49 +1435,50 @@ var TimerEvent = {
 			var child;
 			var maxWidth = 0;
 			var maxHeight = 0;
-			if(this.direction=="vertical"){
-				for(var i=0;i<numChildren;i++){
+			if(this.direction == "vertical") {
+				for(var i = 0; i < numChildren; i++) {
 					child = children[i];
-					child.stageX = this.paddingLeft+this.stageX;
+					child.stageX = this.paddingLeft + this.stageX;
 					child.x = this.paddingLeft;
 					child.y = this.measureHeight;
-					child.stageY = this.stageY+child.y;
+					child.stageY = this.stageY + child.y;
 					if(i == 0) {
 						child.y += this.paddingTop;
-					}else{
-						child.y+=this.verticalGap;
+					} else {
+						child.y += this.verticalGap;
 					}
 					if(child.explicitOrMeasureWidth > maxWidth) {
 						maxWidth = child.explicitOrMeasureWidth;
 					}
 					this.measureHeight = child.y + child.explicitOrMeasureHeight;
 				}
-				this.measureWidth = maxWidth+this.paddingLeft+this.paddingRight;
-				this.measureHeight+= this.paddingBottom;
-			}else{
-				for (var i=0; i < numChildren; i++) {
+				this.measureWidth = maxWidth + this.paddingLeft + this.paddingRight;
+				this.measureHeight += this.paddingBottom;
+			} else {
+				for(var i = 0; i < numChildren; i++) {
 					child = children[i];
-					child.stageY = this.paddingTop+this.stageY;
+					child.stageY = this.paddingTop + this.stageY;
 					child.y = this.paddingTop;
 					child.x = this.measureWidth;
-					child.stageX = this.stageX+child.x;
-					if(i==0){
-						child.x += this.paddingLeft;	
-					}else{
-						child.x+=this.horizontalGap;
+					child.stageX = this.stageX + child.x;
+					if(i == 0) {
+						child.x += this.paddingLeft;
+					} else {
+						child.x += this.horizontalGap;
 					}
-					if(child.explicitOrMeasureHeight>maxHeight){
+					if(child.explicitOrMeasureHeight > maxHeight) {
 						maxHeight = child.explicitOrMeasureHeight;
 					}
 					this.measureWidth = child.x + child.explicitOrMeasureWidth;
 				}
-				this.measureHeight = maxHeight+this.paddingTop+this.paddingBottom;
-				this.measureWidth+= this.paddingRight;
+				this.measureHeight = maxHeight + this.paddingTop + this.paddingBottom;
+				this.measureWidth += this.paddingRight;
 			}
 		}
 	}
-	
+
 })();
+
 
 //---------------------------------------------
 (function() {
---
+++ demos/scripts/group.js
@@ -0,0 +1,48 @@
+(function(){
+	var stage = new Flex.Stage("myCanvas");
+	var bmd = new Flex.BitmapData("http://nodejs.org/images/logo.png");
+	var bitmap = new Flex.Bitmap(bmd);
+	stage.addChild(bitmap);
+	
+	var group1 = new Flex.Group();
+	for(var i=0;i<3;i++){
+		bmd = new Flex.BitmapData("http://nodejs.org/images/logo.png");
+		bitmap = new Flex.Bitmap(bmd);
+		bitmap.width = 200;
+		bitmap.height = 50;
+		group1.addChild(bitmap);
+	}
+	
+	group1.x = 400;
+	stage.addChild(group1);
+	
+	var group2 = new Flex.Group();
+	group2.direction = "horizontal";
+	for(var i=0;i<3;i++){
+		bmd = new Flex.BitmapData("http://nodejs.org/images/logo.png");
+		bitmap = new Flex.Bitmap(bmd);
+		bitmap.width = 200;
+		bitmap.height = 50;
+		group2.addChild(bitmap);
+	}
+	group2.y = group1.measureHeight+100;
+	stage.addChild(group2);
+	group2.addEventListener("click",function(event){
+		if(group2.direction=="vertical"){
+			group2.direction = "horizontal";
+		}else{
+			group2.direction = "vertical";
+		}
+	});
+	
+	group1.addEventListener("click",clickHandler);
+	function clickHandler(event){
+		trace(event);
+		if(group1.direction=="vertical"){
+			group1.direction = "horizontal";
+		}else{
+			group1.direction = "vertical";
+		}
+		
+	}
+})();
